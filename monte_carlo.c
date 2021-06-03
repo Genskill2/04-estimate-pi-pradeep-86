ab2beec0198e97c1f8d89a820e860ebcceb5dde0
pradeep-86 <84574355+pradeep-86@users.noreply.github.com>
2021-06-03 23:47:19 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,41 +3,22 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+float mc_pi(int x);
 
-float frandom() {
-  long int q = random();
-  float ret = (float)q/(float)RAND_MAX;
-  return ret;
-}
+float c= 0.0
 
-int main(void) {
-  float pi0;
-  float pi1;
+for(int i=0;i<x;i++){
+  float x= frandom();
+  float y=frandom();
+  float d=sqrt((x*x)+(y*y));
   
-  pi0 = mc_pi(25000);
-  pi1 = mc_pi(25000);
-  printf("%f %f\n", pi0, pi1);
+  if(d<=1){
+    c++;
+  }}
+float ans= 4.0*(c/(float)x);
+return (ans)
   
-  if (pi0 == pi1) {
-      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
-      abort();
-    }
-
-  if (fabs(pi0 - pi1) > 0.05) {
-      printf("Two separate estimates %f and %f are too different.\n", pi0, pi1);
-      abort();
-  }
-
-    
-  for (int i=2000; i<5000; i++) {
-    pi0 = mc_pi(i);
-    if (!(fabs(pi0 - M_PI) < 0.4)) {
-      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi0);
-      abort();
-    }
-  }
-}
+  
 
 
 
